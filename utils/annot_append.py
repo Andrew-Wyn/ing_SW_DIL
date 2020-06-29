476f162dcaddf424f357af10c9468da5607a93d5
Simone Cimarelli <aquilairreale@ymail.com>
2020-06-29 17:26:54 +0200

    Create annot_append.py

---
+++ utils/annot_append.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# na strunzata galattica
+import json
+import sys
+
+# open the input file and parse the correct informations
+with open(sys.argv[1]) as in_file:
+    in_json = json.load(in_file)
+
+# append the informations to the output file
+with open(sys.argv[2]) as out_file:
+    out_json = json.load(out_file)
+
+for k, v in in_json["_via_img_metadata"].items():
+    out_json[k] = v
+
+with open(sys.argv[2], "w") as f:
+    json.dump(out_json, f, indent=4)
