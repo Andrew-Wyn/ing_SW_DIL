76dc1b4915e291f301dc9f8b7a2b211799a88bed
Simone Cimarelli <aquilaireale@ymail.com>
2020-07-02 20:22:00 +0200

    Fix whitespace

--- src/document_CNN.py
+++ src/document_CNN.py
@@ -18,7 +18,8 @@ OcrRecord = namedtuple("OcrRecord", "conf,text,x,y,w,h")
 
 
 class InferenceConfig(Config):
-    """Configuration for training on the toy  dataset.
+    """
+    Configuration for training on the toy dataset.
     Derives from the base Config class and overrides some values.
     """
     # Give the configuration a recognizable name
@@ -58,16 +59,15 @@ class InferenceConfig(Config):
 # }
 
 def check_box(snap_w, snap_h, ocr_x1, ocr_y1, ocr_w, ocr_h, x1, y1, x2, y2):
-
     x1 = int(snap_w*x1)
     x2 = int(snap_w*x2)
     y1 = int(snap_h*y1)
     y2 = int(snap_h*y2)
-    
+
     ocr_x2 = ocr_x1 + ocr_w - 1
     ocr_y2 = ocr_y1 + ocr_h - 1
 
-    return x1 <= ocr_x1 and ocr_x2 <= x2 and y1 <= ocr_y1 and ocr_y2 <= y2     
+    return x1 <= ocr_x1 and ocr_x2 <= x2 and y1 <= ocr_y1 and ocr_y2 <= y2
 
 
 class Detectron:
@@ -89,8 +89,6 @@ class Detectron:
                     region["rule"] = re.compile(region["rule"], re.IGNORECASE)
 
     def recognize(self, image):
-        # Run object detection
-
         image = np.frombuffer(image, np.uint8)
         image = cv2.imdecode(image, cv2.IMREAD_COLOR)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -103,23 +101,19 @@ class Detectron:
 
         ret = []
 
-        
-
         for roi, class_id in zip(r["rois"], r["class_ids"]):
             class_name = self._class_names[class_id-1]
             y1, x1, y2, x2 = roi
             snapshot = image[y1:y2+1,x1:x2+1,:]
             snapshot_h, snapshot_w, _ = snapshot.shape
             ocr_data = image_to_data(snapshot, lang=self._classes[class_name]["lang"], output_type=Output.DICT)
-            
-            ocr_data["conf"] = [int(c) for c in ocr_data["conf"]]            
+
+            ocr_data["conf"] = [int(c) for c in ocr_data["conf"]]
             ocr_data = [OcrRecord(*t) for t in zip(ocr_data["conf"], ocr_data["text"], ocr_data["top"], ocr_data["left"], ocr_data["width"], ocr_data["height"])]
             ocr_data = [ocr for ocr in ocr_data if ocr.conf >= self._classes[class_name]["min_conf"]]
 
             snapshot = cv2.cvtColor(snapshot, cv2.COLOR_RGB2BGR)
             _, snapshot = cv2.imencode(".png", snapshot)
-
-            
 
             cur_ret = {
                 "type": class_name,
@@ -145,7 +139,7 @@ class Detectron:
                             break
                 else:
                     cur_ret["attributes"][name] = ocr.text
-                    region_useds.add(name)           
+                    region_useds.add(name)
 
             ret.append(cur_ret)
 
