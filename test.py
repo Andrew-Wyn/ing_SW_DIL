98d1ca10e4c3ff71c2e29109dd47f3723dd5face
Andrew-Wyn <terraformer.144@gmail.com>
2020-07-06 12:16:01 +0200

    handled exception bytestream, added handling bg recognition

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import unittest
 from src.document_CNN import Detectron
 
 import json
+import cv2
 
 class TestSet(unittest.TestCase):
     def __init__(self, *args, **kwargs):
@@ -19,8 +20,14 @@ class TestSet(unittest.TestCase):
         """
         # data = {}
 
-        result = None
-        self.assertIsNone(result)
+        with open("test/test_tesserino.jpg", "rb") as f:
+            image = f.read()
+
+
+        result = self._model.recognize(image)
+        attributes = result[0]["attributes"]
+        expected = json.loads('{"Matricola": ["311279"], "Nome": ["Luca", "Moroni"]}')
+        self.assertEqual(attributes, expected)
 
     def test_patente(self):
         """
@@ -28,8 +35,14 @@ class TestSet(unittest.TestCase):
         """
         # data = {}
 
-        result = None
-        self.assertIsNone(result)
+        with open("test/test_backgroud.jpg", "rb") as f:
+            image = f.read()
+
+        result = self._model.recognize(image)
+
+        expected = []
+
+        self.assertEqual(result, expected)
     
     def test_bg(self):
         """
@@ -39,6 +52,17 @@ class TestSet(unittest.TestCase):
 
         result = None
         self.assertIsNone(result)
+    
+    def test_bytestream(self):
+        """
+        test bytestream that don't represent a png image
+        """
+
+        bytestream = b'nasdasd'
+
+        with self.assertRaises(cv2.error):
+            result = self._model.recognize(bytestream)
+
 
 if __name__ == '__main__':
     unittest.main()
